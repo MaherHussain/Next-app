4fc805931058e4ac7acb5efbf36b9ef74f91c4ac
Maher Hussain <maherhussain6@gmail.com>
2025-06-20 00:18:14 +0200

    fix(Cart): show empty message in the cart page

--- my-next-app/src/app/utils/helpers/helpers.ts
+++ my-next-app/src/app/utils/helpers/helpers.ts
@@ -1,8 +1,11 @@
 export function PriceFormatter(price: number) {
-    return (price).toLocaleString("da-DK", {
-        style: "currency",
-        currency: "DKK",
-    })
+    if (price) {
+        return (price).toLocaleString("da-DK", {
+            style: "currency",
+            currency: "DKK",
+        })
+    }
+
 
 }
 export const getCartIdFromLocalStorage = () => {
