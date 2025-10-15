72f848e499a0334aa22dc2758dccd23d7a30b75a
Maher Hussain <maherhussain6@gmail.com>
2025-10-15 09:23:20 +0200

    feat: add new product
    Changes are adding a new product and showing product list for the
    admin/partner

---
+++ my-next-app/src/app/partner/dashboard/products/components/index.ts
@@ -0,0 +1,2 @@
+export { default as ProductAddModal } from "./product-add-modal";
+export { default as ProductList } from "./product-list";
--- my-next-app/src/app/queries/products.ts
+++ my-next-app/src/app/queries/products.ts
@@ -1,10 +1,23 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { getProducts } from '../services/product-services'
+import { addProduct, getProducts } from '../services/product-services'
 
 export function useGetProducts() {
     return useQuery({
         queryKey: ['products'],
         queryFn: getProducts,
         retry: 3
+    })
+}
+export function useAddProduct() {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: addProduct,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['products'] });
+        },
+        onError: (err: any) => {
+            const errorMessage = err?.response?.data?.message || err?.message || "An error occurred";
+            console.log(errorMessage)
+        }
     })
 }
