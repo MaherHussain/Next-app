bc7330b22b048fb43295c73bf345b2faabaef2fa
Maher Hussain <maherhussain6@gmail.com>
2025-10-17 06:25:29 +0200

    feat: edit product

--- my-next-app/src/app/queries/products.ts
+++ my-next-app/src/app/queries/products.ts
@@ -1,5 +1,5 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { addProduct, getProducts, deleteProduct } from '../services/product-services'
+import { addProduct, getProducts, deleteProduct, editProduct } from '../services/product-services'
 
 export function useGetProducts() {
     return useQuery({
@@ -26,6 +26,20 @@ export function useDeleteProduct() {
     const queryClient = useQueryClient();
     return useMutation({
         mutationFn: deleteProduct,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['products'] });
+        },
+        onError: (err: any) => {
+            const errorMessage = err?.response?.data?.message || err?.message || "An error occurred";
+            console.log(errorMessage)
+        }
+    })
+}
+
+export function useEditProduct() {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: ({ product }: { product: { id: string, name?: string; price?: number } }) => editProduct(product),
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ['products'] });
         },
